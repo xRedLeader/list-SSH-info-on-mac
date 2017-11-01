95ab0e12091520c1b11a6e0ae3fe817cf5fe9f17
Brenden Smith <smithbc@unityonline.org>
2017-11-01 08:58:44 -0500

    upgrade script to select host when specified

--- sshInfo.sh
+++ sshInfo.sh
@@ -1,30 +1,118 @@
 #!/bin/bash
-file="$HOME/.ssh/config"
+VERSION="1.0.0"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename=$1
+                                ;;
+        -H | --host )           shift
+                                host=$1
+                                ;;
+        -v | --version )        echo "Version: $VERSION"
+                                exit
+                                ;;
+        -h | --help ) 
+            printf "\n"
+            echo "Usage: my_test [OPTIONS]"
+            echo "OPTION includes:"
+            echo "     -f | --file -sets the .ssh config file if you have placed it 
+            anywhere than $HOME/.ssh/config"
+            echo "     -v | --version - prints out version information for this script"
+            echo "     -H | --host - prints options from specific host"
+            echo "     -h | --help - displays this message"
+            printf "\n"
+            exit
+            ;;
+        * ) 
+            printf "\n"
+            echo "Invalid option: $1"
+            echo "Usage: sshInfo [OPTIONS]"
+            echo "     -f | --file -sets the .ssh config file if you have placed it 
+            anywhere than $HOME/.ssh/config"
+            echo "     -v | --version - prints out version information for this script"
+            echo "     -H | --host - prints options from specific host"
+            echo "     -h | --help - displays this message"
+            printf "\n"
+            exit
+            ;;
+    esac
+    shift
+done
+
+
+# set the location of the config file if not passed in, try default location
+if [ "${filename}" != "" ] ; then
+    file=${filename}
+else
+    file="$HOME/.ssh/config"
+fi
+
+
+# read in the file
 increment=0
+
 while IFS= read -r line
 do
-	if [[ $line = \#* ]] ; then
-    	filler="gone"
+    # ignore all commented lines
+    if [[ $line = \#* ]] ; then
+        filler="gone"
     else
-    	#need to do values
-    	#echo "okay"
-    	arrLine=(${line//;/ })
-    	#echo ${arrLine[1]}
-    	arrayOptions[$increment]=${arrLine[0]}
-    	arrayValues[$increment]=${arrLine[1]}
-    	#echo ${array[0]}
-	fi
-	increment=$((increment+1))
+        # Read in line
+        arrLine=(${line//;/ })
+
+        # set the Options = to array at index of counter
+        arrayOptions[$increment]=${arrLine[0]}
+        arrayValues[$increment]=${arrLine[1]}
+
+    fi
+    # increase counter
+    increment=$((increment+1))
 done <"$file"
 
+
+# print first bar
 printf "\n"
 echo '--------------------------------------------'
 
-for i in "${!arrayOptions[@]}"; do
-	printf "%-25s %s \n" "${arrayOptions[$i]}" "${arrayValues[$i]}"
+# if someone specified a host name, only print that out, otherwise, print them all out.
+if [ "${host}" != "" ] ; then
+    increment=0
 
-	if [ ${arrayOptions[$i]} == "IdentityFile" ] || [ ${arrayValues[$i]} == "password" ] ; then
-		echo '--------------------------------------------'
-	fi 
-done
-printf "\n"
+    # loop through array and find the Host specified
+    for i in "${!arrayValues[@]}"; do
+        if [[ "${arrayValues[$increment]}" == "${host}" ]]; then
+            while :; do
+                # print out this option of the Host that was wanted
+                printf "%-25s %s \n" "${arrayOptions[$increment]}" "${arrayValues[$increment]}"
+                increment=$((increment+1))
+
+                if [ "${arrayOptions[$increment]}" == "Host" ]; then
+
+                    echo '--------------------------------------------'
+                    printf "\n"
+                    break
+                fi
+            done
+        fi
+        increment=$((increment+1))
+    done
+
+else
+    # loop through all configh file and print out options
+    for i in "${!arrayOptions[@]}"; do
+        printf "%-25s %s \n" "${arrayOptions[$i]}" "${arrayValues[$i]}"
+
+        if [ ${arrayOptions[$i]} == "IdentityFile" ] || [ ${arrayValues[$i]} == "password" ] ; then
+            echo '--------------------------------------------'
+        fi 
+    done
+    printf "\n"
+fi
+
+
+
+
+
+
+
